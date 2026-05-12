10eb23a5351a47d4a9b755881d8715522edbe12d
Neel Singh <156997262+NeelSingh16@users.noreply.github.com>
2026-05-12 21:35:54 +0530

    Update DMVExp1.py

--- DMVExp1.py
+++ DMVExp1.py
@@ -7,9 +7,9 @@ import seaborn as sns
 # 1. Load Data
 # -------------------------
 # Load sales data from CSV, Excel, and JSON
-csv_data = pd.read_csv("./datasets/sales_data_sample_2.csv", encoding="cp1252")
-excel_data = pd.read_excel("./datasets/sales_data_sample_2.xlsx")
-json_data = pd.read_json("./datasets/sales_data_sample_2.json", lines=True)
+csv_data = pd.read_csv("./sales_data_sample_2.csv", encoding="cp1252")
+excel_data = pd.read_excel("./sales_data_sample_2.xlsx")
+json_data = pd.read_json("./sales_data_sample_2.json", lines=True)
 
 print("CSV Data Sample:\n", csv_data.head())
 print("\nExcel Data Sample:\n", excel_data.head())
